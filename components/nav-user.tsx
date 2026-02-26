73e45e5b55b248320f822e9b85677b55f3291185
SnowyWalk <chunsa1166jp@gmail.com>
2026-02-26 13:36:17 +0900

    프로필 꾸미기 및 로그인 로그아웃 기능 추가

--- components/nav-user.tsx
+++ components/nav-user.tsx
@@ -4,7 +4,7 @@ import {
     BadgeCheck,
     Bell,
     ChevronsUpDown,
-    CreditCard, Ghost,
+    CreditCard, Ghost, LogIn,
     LogOut, Moon,
     Sparkles, Sun,
 } from "lucide-react"
@@ -32,8 +32,9 @@ import {
 import {toggleThemeWithTransition} from "@/components/ui/animated-theme-toggler";
 import {useTheme} from "next-themes";
 import {useMe} from "@/hooks/use-me";
-import {useSession} from "next-auth/react";
+import {signOut, useSession} from "next-auth/react";
 import {Skeleton} from "@/components/ui/skeleton";
+import {useRouter} from "next/navigation";
 
 export function NavUser({
                             user,
@@ -49,8 +50,9 @@ export function NavUser({
     const {resolvedTheme, setTheme} = useTheme()
     const isDark = resolvedTheme === "dark"
 
-    const {status: sessionStatus} = useSession();
-    const {data: me, isLoading: isMeLoading} = useMe()
+    const {status: sessionStatus, update: sessionUpdate} = useSession();
+    const {data: me, isLoading: isMeLoading, resetMe} = useMe()
+    const router = useRouter();
 
     const UserProfile = () => {
         if (isMeLoading || sessionStatus == "loading")
@@ -70,10 +72,10 @@ export function NavUser({
             return (
                 <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                     <Avatar className="h-8 w-8 rounded-lg" key="nav-user-avatar-guest">
-                        <AvatarFallback><Ghost/></AvatarFallback>
+                        <AvatarFallback><Ghost strokeWidth={1.5}/></AvatarFallback>
                     </Avatar>
                     <div className="grid flex-1 text-left text-sm leading-tight">
-                        <span className="truncate font-medium">게스트</span>
+                        <span className="truncate font-medium text-muted-foreground">게스트</span>
                         <span className="truncate text-xs">비로그인 상태</span>
                     </div>
                 </div>
@@ -91,6 +93,87 @@ export function NavUser({
                 </div>
             </div>
         )
+    }
+
+    const ThemeChangeMenuItem = () => {
+        return (
+            <DropdownMenuItem
+                onClick={(event) => {
+                    void toggleThemeWithTransition(event.currentTarget as HTMLElement,
+                        {
+                            isDark: isDark,
+                            duration: 1000,
+                            cursorX: event.clientX,
+                            cursorY: event.clientY,
+                            onThemeChange: () => setTheme(isDark ? "light" : "dark")
+                        }
+                    )
+                }}
+            >
+                {isDark ? <Sun/> : <Moon/>}
+                테마 변경
+            </DropdownMenuItem>
+        )
+    }
+
+    const UserMenuItems = () => {
+        return (
+            <>
+                <DropdownMenuLabel className="p-0 font-normal">
+                    {UserProfile()}
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator/>
+                <DropdownMenuGroup>
+                    <DropdownMenuItem>
+                        <Sparkles/>
+                        Upgrade to Pro
+                    </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator/>
+                <DropdownMenuGroup>
+                    <DropdownMenuItem>
+                        <BadgeCheck/>
+                        Account
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                        <CreditCard/>
+                        Billing
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                        <Bell/>
+                        Notifications
+                    </DropdownMenuItem>
+                    {ThemeChangeMenuItem()}
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator/>
+                <DropdownMenuItem onClick={async () => {
+                    await signOut({redirect: false});
+                    await sessionUpdate();
+                    resetMe();
+                    router.refresh();
+                }
+                }>
+                    <LogOut/>
+                    Log out
+                </DropdownMenuItem>
+            </>
+        )
+    }
+
+    const GuestMenuItems = () => {
+        return (
+            <>
+                <DropdownMenuLabel className="p-0 font-normal">
+                    {UserProfile()}
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator/>
+                <ThemeChangeMenuItem/>
+                <DropdownMenuSeparator/>
+                <DropdownMenuItem onClick={() => router.push("/login-start")}>
+                    <LogIn/>
+                    Log in
+                </DropdownMenuItem>
+            </>)
     }
 
     return (
@@ -113,52 +196,8 @@ export function NavUser({
                         align="end"
                         sideOffset={4}
                     >
-                        <DropdownMenuLabel className="p-0 font-normal">
-                            {UserProfile()}
-                        </DropdownMenuLabel>
-                        <DropdownMenuSeparator/>
-                        <DropdownMenuGroup>
-                            <DropdownMenuItem>
-                                <Sparkles/>
-                                Upgrade to Pro
-                            </DropdownMenuItem>
-                        </DropdownMenuGroup>
-                        <DropdownMenuSeparator/>
-                        <DropdownMenuGroup>
-                            <DropdownMenuItem>
-                                <BadgeCheck/>
-                                Account
-                            </DropdownMenuItem>
-                            <DropdownMenuItem>
-                                <CreditCard/>
-                                Billing
-                            </DropdownMenuItem>
-                            <DropdownMenuItem>
-                                <Bell/>
-                                Notifications
-                            </DropdownMenuItem>
-                            <DropdownMenuItem
-                                onClick={(event) => {
-                                    void toggleThemeWithTransition(event.currentTarget as HTMLElement,
-                                        {
-                                            isDark: isDark,
-                                            duration: 1000,
-                                            cursorX: event.clientX,
-                                            cursorY: event.clientY,
-                                            onThemeChange: () => setTheme(isDark ? "light" : "dark")
-                                        }
-                                    )
-                                }}
-                            >
-                                {isDark ? <Sun/> : <Moon/>}
-                                테마 변경
-                            </DropdownMenuItem>
-                        </DropdownMenuGroup>
-                        <DropdownMenuSeparator/>
-                        <DropdownMenuItem>
-                            <LogOut/>
-                            Log out
-                        </DropdownMenuItem>
+                        {me && UserMenuItems()}
+                        {!me && GuestMenuItems()}
                     </DropdownMenuContent>
                 </DropdownMenu>
             </SidebarMenuItem>
